9022e93b8cc3dbcb0310ea2e9daf4648468e810d
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-05-02 13:03:06 +0600

    eslint-config: disable global-require

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -1,3 +1,6 @@
+// Disable quote-props to have one style in all config
+/* eslint quote-props: "off" */
+
 const err = 'error'
 const warn = 'warn'
 const off = 'off'
@@ -7,11 +10,12 @@ module.exports = {
     'airbnb-base',
   ],
   rules: {
-    semi: [err, 'never'],
+    'semi': [err, 'never'],
     'arrow-parens': [warn, 'as-needed'],
     'id-length': [err, { min: 1, max: 40 }],
     'no-multiple-empty-lines': [warn, { max: 5 }],
     'arrow-body-style': off,
     'no-console': off,
+    'global-require': off,
   },
 }
