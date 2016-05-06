bb8be93d4affa97682f24838a677fcb62d3b8fe1
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-05-06 15:38:54 +0600

    Add more options to package.json

--- generator/generators/app/test.js
+++ generator/generators/app/test.js
@@ -1,3 +1,5 @@
+/* eslint-disable func-names */
+
 const path = require('path')
 // const R = require('ramda')
 const assert = require('yeoman-assert')
@@ -6,15 +8,18 @@ require('chai').should()
 
 
 const defaults = {
-  appName: 'appName',
+  appName: 'app-name',
   appDesc: 'Your awesome app!',
   appKeywords: 'some, keywords',
+  name: 'max',
+  email: 'max@company.com',
+  appLicense: 'BSD',
 }
 
 it('generates expected files', done => {
   helpers.run(__dirname)
     .withPrompts(defaults)
-    .on('end', function () {
+    .on('end', () => {
       assert.file([
         '.babelrc',
         '.editorconfig',
@@ -30,11 +35,36 @@ it('generates expected files', done => {
         'app/helpers/requireAll.js',
         'app/helpers/string.js',
       ])
-      assert.fileContent('README.md', `# ${defaults.appName}`)
-      assert.fileContent('README.md', `> ${defaults.appDesc}`)
+      done()
+    })
+  .on('error', done)
+})
+
+
+it('generates README right', done => {
+  helpers.run(__dirname)
+    .withPrompts(defaults)
+    .on('end', () => {
+      assert.fileContent('README.md', '# app-name')
+      assert.fileContent('README.md', '> Your awesome app!')
+
+      done()
+    })
+  .on('error', done)
+})
+
+
+it('generates package.json right', done => {
+  helpers.run(__dirname)
+    .withPrompts(defaults)
+    .on('end', function () {
       const packageJson = require(path.join(this.env.cwd, 'package.json'))
-      packageJson.name.should.be.equal('appName')
-      packageJson.description.should.be.equal(defaults.appDesc)
+      packageJson.name.should.be.equal('app-name')
+      packageJson.description.should.be.equal('Your awesome app!')
+      packageJson.author.should.be.equal('max <max@company.com>')
+      packageJson.keywords.should.be.deep.equal('some, keywords')
+      packageJson.license.should.be.equal('BSD')
+
       done()
     })
   .on('error', done)
