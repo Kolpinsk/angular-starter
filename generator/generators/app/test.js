4d8987c4013fa72d5944aee26ef6d94701c070ba
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-05-06 17:10:50 +0600

    generator: wrap tests in describe

--- generator/generators/app/test.js
+++ generator/generators/app/test.js
@@ -13,86 +13,88 @@ const defaults = {
 }
 
 
-it('generates expected files', done => {
-  helpers.run(__dirname)
-    .withPrompts(defaults)
-    .on('end', () => {
-      assert.file([
-        '.babelrc',
-        '.editorconfig',
-        '.eslintrc.yml',
-        '.gitignore',
-        'package.json',
-        'README.md',
-        'server.js',
-        'webpack.conf.js',
-        'app/app.js',
-        'app/helpers/constants.json',
-        'app/helpers/index.js',
-        'app/helpers/requireAll.js',
-        'app/helpers/string.js',
-      ])
-      done()
-    })
-  .on('error', done)
-})
-
-
-it('generates routes right', done => {
-  helpers.run(__dirname)
-    .withPrompts(defaults)
-    .on('end', () => {
-      assert.file('app/pages/routes.js')
-      assert.fileContent('app/app.js', `\n  require('./pages/routes').default,\n`)
-      done()
-    })
-  .on('error', done)
-})
-
-
-it('not generates routes if withRouting === false', done => {
-  helpers.run(__dirname)
-    .withPrompts(Object.assign(defaults, { withRouting: false }))
-    .on('end', () => {
-      assert.noFile('app/pages/routes.js')
-      assert.noFileContent([
-        ['app/app.js', 'require(\'./pages/routes\')'],
-        ['package.json', 'angular-ui-router'],
-        ['package.json', '\n    \n'],
-      ])
-      assert.fileContent('app/app.js', `\n  ...services,\n  //`)
-      done()
-    })
-  .on('error', done)
-})
-
-
-it('generates README right', done => {
-  helpers.run(__dirname)
-    .withPrompts(defaults)
-    .on('end', () => {
-      assert.fileContent([
-        ['README.md', '# app-name'],
-        ['README.md', '> Your awesome app!'],
-      ])
-      done()
-    })
-  .on('error', done)
-})
-
-
-it('generates package.json right', done => {
-  helpers.run(__dirname)
-    .withPrompts(defaults)
-    .on('end', () => {
-      assert.jsonFileContent('package.json', {
-        name: 'app-name',
-        description: 'Your awesome app!',
-        author: 'max <max@company.com>',
-        keywords: 'some, keywords',
-        license: 'BSD',
+describe('app', () => {
+  it('generates expected files', done => {
+    helpers.run(__dirname)
+      .withPrompts(defaults)
+      .on('end', () => {
+        assert.file([
+          '.babelrc',
+          '.editorconfig',
+          '.eslintrc.yml',
+          '.gitignore',
+          'package.json',
+          'README.md',
+          'server.js',
+          'webpack.conf.js',
+          'app/app.js',
+          'app/helpers/constants.json',
+          'app/helpers/index.js',
+          'app/helpers/requireAll.js',
+          'app/helpers/string.js',
+        ])
+        done()
       })
-      done()
-    })
-  .on('error', done)
+    .on('error', done)
+  })
+
+
+  it('generates routes right', done => {
+    helpers.run(__dirname)
+      .withPrompts(defaults)
+      .on('end', () => {
+        assert.file('app/pages/routes.js')
+        assert.fileContent('app/app.js', `\n  require('./pages/routes').default,\n`)
+        done()
+      })
+    .on('error', done)
+  })
+
+
+  it('not generates routes if withRouting === false', done => {
+    helpers.run(__dirname)
+      .withPrompts(Object.assign(defaults, { withRouting: false }))
+      .on('end', () => {
+        assert.noFile('app/pages/routes.js')
+        assert.noFileContent([
+          ['app/app.js', 'require(\'./pages/routes\')'],
+          ['package.json', 'angular-ui-router'],
+          ['package.json', '\n    \n'],
+        ])
+        assert.fileContent('app/app.js', `\n  ...services,\n  //`)
+        done()
+      })
+    .on('error', done)
+  })
+
+
+  it('generates README right', done => {
+    helpers.run(__dirname)
+      .withPrompts(defaults)
+      .on('end', () => {
+        assert.fileContent([
+          ['README.md', '# app-name'],
+          ['README.md', '> Your awesome app!'],
+        ])
+        done()
+      })
+    .on('error', done)
+  })
+
+
+  it('generates package.json right', done => {
+    helpers.run(__dirname)
+      .withPrompts(defaults)
+      .on('end', () => {
+        assert.jsonFileContent('package.json', {
+          name: 'app-name',
+          description: 'Your awesome app!',
+          author: 'max <max@company.com>',
+          keywords: 'some, keywords',
+          license: 'BSD',
+        })
+        done()
+      })
+    .on('error', done)
+  })
 })
