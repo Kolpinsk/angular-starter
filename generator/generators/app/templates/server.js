a5a3c7b7ce37659bc245413d82e9d36a3972db8d
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-05-08 23:02:28 +0600

    refactor(generator-app): move webpack reporter to another module

--- generator/generators/app/templates/server.js
+++ generator/generators/app/templates/server.js
@@ -12,48 +12,8 @@ const compiler = webpack(webpackConfig)
 
 const app = express()
 
-
-const oraReporter = () => {
-  const spinner = ora()
-  spinner.start()
-  spinner.pause = () => {
-    spinner.stop()
-    const frames = spinner.spinner.frames
-    const index = spinner.frameIndex
-    const oldFrame = frames[index]
-    frames[index] = '⠿'
-    spinner.render()
-    frames[index] = oldFrame
-  }
-
-  return ({ state, stats }) => {
-    if (state) {
-      const durations = stats.endTime - stats.startTime
-      const formatedDurations = durations >= 1000 ? `${durations / 1000} s` : `${durations} ms`
-      const message = `Completed in ${formatedDurations.magenta}`
-
-      if (stats.hasErrors() || stats.hasWarnings()) {
-        spinner.clear()
-        const { errors, warnings } = stats.compilation
-        errors.concat(warnings).forEach(warning => {
-          if (warning.message) {
-            console.log(warning.message)
-          }
-        })
-      }
-
-      spinner.text = message
-      spinner.pause()
-    } else {
-      spinner.start()
-      spinner.text = 'Building...'
-    }
-  }
-}
-
-
 app.use(require('webpack-dev-middleware')(compiler, {
-  reporter: oraReporter(),
+  reporter: require('webpack-dots-reporter')(),
 }))
 app.use(require('webpack-hot-middleware')(compiler, { log: () => 1 }))
 app.use(express.static('build/', { index: false }))
