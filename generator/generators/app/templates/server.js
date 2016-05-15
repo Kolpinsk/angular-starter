2b1a994565c1c94e5160e5388233eb688e8bfb19
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-05-16 01:21:51 +0600

    feat(generator-app): add styleguide

--- generator/generators/app/templates/server.js
+++ generator/generators/app/templates/server.js
@@ -1,23 +1,36 @@
+const path = require('path')
 const express = require('express')
 const webpack = require('webpack')
-const { findAPortNotInUse } = require('portscanner')
 const constants = require('./app/helpers/constants.json')
-require('colors')
+const { findAPortNotInUse } = require('portscanner')
+const styleguide = require('styleguide-as')
 
+
+// init app
+const app = express()
+app.set('view engine', 'jade')
+
+
+// compile static through webpack middlewares
 const webpackConfig = require('./webpack.conf')
 webpackConfig.entry = [
   'webpack-hot-middleware/client?reload=true',
 ].concat(webpackConfig.entry)
 const compiler = webpack(webpackConfig)
 
-const app = express()
-
 app.use(require('webpack-dev-middleware')(compiler, {
   reporter: require('webpack-dots-reporter')(),
 }))
 app.use(require('webpack-hot-middleware')(compiler, { log: () => 1 }))
-app.use(express.static('build/', { index: false }))
-app.get('*', (req, res) => res.render('index.jade', constants))
+
+
+
+app.use('/styleguide', styleguide({
+  componentsDir: path.join(__dirname, './app/components'),
+  constants,
+}))
+
+app.get('*', (req, res) => res.render('index', { constants }))
 
 findAPortNotInUse(3000, 3010, 'localhost', (err, port) => {
   if (err) console.error(err)
@@ -27,5 +40,3 @@ findAPortNotInUse(3000, 3010, 'localhost', (err, port) => {
     console.log(`http://localhost:${port}`)
   })
 })
-
-exports.app = app
